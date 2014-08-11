0a5a42908f8a0e9d35b8a5a61afa6559f4615f42
Robin Oster <hugowalker@gmail.com>
2014-08-11 17:15:11 +0200

    Included new Alamofire Networking library and changed the example code

--- ROJSONParsing/BaseWebservice.swift
+++ ROJSONParsing/BaseWebservice.swift
@@ -10,62 +10,28 @@ import Foundation
 
 class BaseWebservice {
 
-    func get(urlString: String, callback: (Int, AnyObject!) -> ()) {
-        
-        // Make the asynchronous call
-        var sessionConfig = NSURLSessionConfiguration.ephemeralSessionConfiguration()
-        sessionConfig.timeoutIntervalForRequest = 60
-        sessionConfig.timeoutIntervalForResource = 60
-        sessionConfig.requestCachePolicy = NSURLRequestCachePolicy.ReloadIgnoringLocalCacheData
-        
-        var session = NSURLSession(configuration: sessionConfig)
-        
-        var completionHandler = { (data: NSData?, urlResponse: NSURLResponse?, error: NSError?) -> () in
-            
-            if let httpResponse = urlResponse as? NSHTTPURLResponse {
-                if(error == nil && httpResponse.statusCode == 200) {
-                    
-                    var jsonParsingError: NSError?
-                    
-                    var unwrappedJSON : AnyObject! = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &jsonParsingError)
-                    
-                    // If the JSON could be parsed, map it into a dict or an array
-                    if jsonParsingError == nil {
-                        
-                        // Check if its an NSArray or NSDictionary
-                        if let parsedJSONArray = unwrappedJSON as? [AnyObject] {
-                            // Is an JSON Array
-                            // DDLogWrapper.logVerbose("\(parsedJSONArray)")
-                            callback(httpResponse.statusCode, parsedJSONArray)
-                        } else {
-                            if let parsedJSONDict = unwrappedJSON as? NSDictionary {
-                                // If the structure is a Dictionary wrap it into an array
-                                // var arrayWrapper = AnyObject[]()
-                                // arrayWrapper.append(parsedJSONDict)
-                                
-                                callback(httpResponse.statusCode, parsedJSONDict)
-                            } else {
-                                // Unknown type
-                                println("Unknown JSON Format type")
-                            }
-                        }
-                    } else {
-                        callback(httpResponse.statusCode, NSString(data: data, encoding: NSUTF8StringEncoding))
-                    }
-                }
-            } else {
-                println("Http Response not correctly received!")
-            }
+    /**
+    *  Get method for fetching the data from the webservice
+    *
+    *  @param urlString:String Urlto the REST webservice entry
+    *  @param callback:Int     HTTP Response status code
+    *  @param AnyObject!       ROJSONObject Object
+    *
+    */
+    func get(urlString:String, callback:(Int, AnyObject!) -> (), parameters:[String : AnyObject!]? = nil) {
+        Alamofire.request(.GET, urlString, parameters: parameters).responseJSON {(request, response, JSON, error) in
+            callback(response!.statusCode, JSON)
         }
-        
-        var url: NSURL? = NSURL(string : urlString)
-        
-        session.dataTaskWithURL(url, completionHandler: completionHandler).resume()
-        
-        // Pass the data through the callback back to the sender
-        
     }
     
+    /**
+    *  Generic get method which creates automatically the ROJSONObjects from the given JSON response
+    *
+    *  @param urlString:String Url to the REST webservice entry
+    *  @param Int              HTTP Response status code
+    *  @param T                ROJSONObject Object
+    *
+    */
     func get<T: ROJSONObject>(urlString:String, callback: (Int, T) -> ()) {
         var webserviceCallback = {(status:Int, response:AnyObject!) -> () in
             callback(status, T(jsonData: response))
